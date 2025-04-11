335aa9b32345375dec7c1ea25988218e7e265af6
Aniketh <aaatipamula@gmail.com>
2025-04-11 13:47:06 -0500

    set colors for lualine

--- lua/lualine/themes/dracula-nvim.lua
+++ lua/lualine/themes/dracula-nvim.lua
@@ -2,21 +2,21 @@ local dracula = require("dracula")
 local configs = dracula.configs()
 local colors = dracula.colors()
 
-local bg = colors.bg
+local bg = configs.lualine_bg_color or colors.black
 
 local normal = {
-   a = { fg = colors.black, bg = colors.purple, gui = 'bold' },
+   a = { fg = colors.white, bg = colors.green, gui = 'bold' },
    b = { fg = colors.purple, bg = bg },
    c = { fg = colors.white, bg = bg },
 }
 
 local command = {
-   a = { fg = colors.black, bg = colors.cyan, gui = 'bold' },
+   a = { fg = colors.white, bg = colors.cyan, gui = 'bold' },
    b = { fg = colors.cyan, bg = bg },
 }
 
 local visual = {
-   a = { fg = colors.black, bg = colors.pink, gui = 'bold' },
+   a = { fg = colors.white, bg = colors.pink, gui = 'bold' },
    b = { fg = colors.pink, bg = bg },
 }
 
@@ -26,13 +26,13 @@ local inactive = {
 }
 
 local replace = {
-   a = { fg = colors.black, bg = colors.yellow, gui = 'bold' },
+   a = { fg = colors.white, bg = colors.yellow, gui = 'bold' },
    b = { fg = colors.yellow, bg = bg },
    c = { fg = colors.white, bg = bg },
 }
 
 local insert = {
-   a = { fg = colors.black, bg = colors.green, gui = 'bold' },
+   a = { fg = colors.white, bg = colors.purple, gui = 'bold' },
    b = { fg = colors.green, bg = bg },
    c = { fg = colors.white, bg = bg },
 }
